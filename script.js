7d53d7302db47fda24e61295e6e46956b9039038
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-07-09 00:06:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
+let players = ['John', 'Mike', 'Sarah'];
+let person = { name: 'Alice', age: 25 };
 
-const person = {
-  name: "John Doe",
-  age: 80,
-};
+let team = players;
+let team1 = [...players]; // Creates a shallow copy of the players array
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+let cap1 = { ...person }; // Creates a shallow copy of the person object
