98c3f81f7094846ad19db8b1ca45e4d6356efc23
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-07-09 00:08:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let players = ['John', 'Mike', 'Sarah'];
-let person = { name: 'Alice', age: 25 };
+const players = ["John", "Bob", "Alice", "Poppy"];
+const person = {
+  name: "John Doe",
+  age: 80,
+};
 
-let team = players;
-let team1 = [...players]; // Creates a shallow copy of the players array
+const team = players;
+const team1 = [...players]; // Creates a shallow copy of the players array
+const cap1 = { ...person }; // Creates a shallow copy of the person object
 
-let cap1 = { ...person }; // Creates a shallow copy of the person object
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
